3cbe7a45e47af5b25bb10b83444475f1a9a84245
Chade-Git <ChadePro@outlook.com>
2025-03-01 19:53:06 +0800

    Added executable file
    Updated code to remove excessive comments.

--- ICWA Assessment Program/ICWA Assessment Program/ICWA Program.cs
+++ ICWA Assessment Program/ICWA Assessment Program/ICWA Program.cs
@@ -1,10 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 
 int x = 0;
 int y = 0;
-string[] directions = { "NORTH", "EAST", "SOUTH", "WEST" }; //Array of the different directions, placed in clockwise order
+string[] directions = { "NORTH", "EAST", "SOUTH", "WEST" }; 
 string f = "null";
 bool loopy = true;
 
@@ -14,30 +12,30 @@ while (loopy == true)
     switch (userInput) 
     {
         case "MOVE":
-            if (f == "NORTH") //Check if facing NORTH
+            if (f == "NORTH") 
             { 
-                if ((y + 1) < 6) //Check if movement would move robot outside boundary
+                if ((y + 1) < 6) 
                 {
                     y += 1;
                 }
             }
-            else if (f == "EAST") //Check if facing EAST
+            else if (f == "EAST") 
             {
-                if ((x + 1) < 6) //Check if movement would move robot outside boundary
+                if ((x + 1) < 6) 
                 {
                     x += 1;
                 }
             }
-            else if (f == "SOUTH") //Check if facing SOUTH
+            else if (f == "SOUTH") 
             {
-                if ((y - 1) > -1) //Check if movement would move robot outside boundary
+                if ((y - 1) > -1) 
                 {
                     y -= 1;
                 }
             }
-            else if (f == "WEST") //Check if facing WEST
+            else if (f == "WEST") 
             {
-                if ((x - 1) > -1) //Check if movement would move robot outside boundary
+                if ((x - 1) > -1) 
                 {
                     x -= 1;
                 }
@@ -48,16 +46,16 @@ while (loopy == true)
             }
             break;
         case "LEFT":
-            if (f == "NORTH" || f == "WEST" || f == "SOUTH" || f == "EAST") //Checks if facing is valid so we can be sure robot has been placed
+            if (f == "NORTH" || f == "WEST" || f == "SOUTH" || f == "EAST") 
             {
-                int facingIndex = Array.IndexOf(directions, f); //gets the index number of the current facing direction in direction array
-                if ((facingIndex - 1) < 0) //checking to see if index number -1 would be out of bounds for array
+                int facingIndex = Array.IndexOf(directions, f); 
+                if ((facingIndex - 1) < 0) 
                 {
-                    f = directions[3]; //if out of bounds, set the index to the end of the array
+                    f = directions[3]; 
                 }
                 else
                 {
-                    f = directions[(facingIndex - 1)]; //if in bounds, set it to the preceeding value in the array
+                    f = directions[(facingIndex - 1)]; 
                 }
             }
             else
@@ -66,16 +64,16 @@ while (loopy == true)
             }
             break;
         case "RIGHT":
-            if (f == "NORTH" || f == "WEST" || f == "SOUTH" || f == "EAST") //Checks if facing is valid so we can be sure robot has been placed
+            if (f == "NORTH" || f == "WEST" || f == "SOUTH" || f == "EAST") 
             {
                 int facingIndex = Array.IndexOf(directions, f);
-                if ((facingIndex + 1) > 3) //inverse of code written for LEFT, checking to see if index number +1 would be out of bounds
+                if ((facingIndex + 1) > 3) 
                 {
-                    f = directions[0]; //if out of bounds, set back to beginning value
+                    f = directions[0]; 
                 }
                 else
                 {
-                    f = directions[(facingIndex + 1)]; //otherwise, move to next value in array
+                    f = directions[(facingIndex + 1)]; 
                 }
             }
             else
@@ -84,7 +82,7 @@ while (loopy == true)
             }
             break;
         case "REPORT": 
-            if (f == "NORTH" || f == "WEST" || f == "SOUTH" || f == "EAST") //Checks if facing is valid so we can be sure robot has been placed
+            if (f == "NORTH" || f == "WEST" || f == "SOUTH" || f == "EAST") 
             {
                 Console.WriteLine("=> Output: " + x + ", " + y + ", " + f); 
             }
@@ -98,23 +96,23 @@ while (loopy == true)
             Console.WriteLine("=> Program ended");
             break;
         default:
-            if (Regex.IsMatch(userInput, @"^PLACE\s[0-5],[0-5],(NORTH|WEST|SOUTH|EAST)")) //checks if user has placed 
+            if (Regex.IsMatch(userInput, @"^PLACE\s[0-5],[0-5],(NORTH|WEST|SOUTH|EAST)")) 
             {
-                string trimmedInput = userInput.Remove(0, 6); //removing "PLACE " to get just the substring of values
-                string[] placedArray = Regex.Split(trimmedInput, "[,]"); //removing commas and splitting into an array of individual values
+                string trimmedInput = userInput.Remove(0, 6); 
+                string[] placedArray = Regex.Split(trimmedInput, "[,]"); 
                 try 
                 {
-                    x = Int32.Parse(placedArray[0]); //assigning retrieved values to x, y and z
+                    x = Int32.Parse(placedArray[0]); 
                     y = Int32.Parse(placedArray[1]);
                     f = placedArray[2];
                 }
                 catch {
-                    Console.WriteLine("=> An unexpected error occurred."); //catch statement in case error occurs - shouldn't be possible due to regex
+                    Console.WriteLine("=> An unexpected error occurred."); 
                 }
             }
             else
             {
-                Console.WriteLine("=> Invalid command"); //catch all error message for unrecognised commands or an invalid PLACE command
+                Console.WriteLine("=> Invalid command"); 
             }
             break;
     }
